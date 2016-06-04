9c6a5e655054c3a86d6a780d260d09d4364ff5f3
Luis Fernandes <sednanref2004@gmail.com>
2016-06-04 01:06:58 -0400

    Added ip to geolocation resolution, still having problems to gather the ip from the client conection.

--- sushi_network/views.py
+++ sushi_network/views.py
@@ -1,7 +1,28 @@
 from django.views import generic
 import foursquare
+import pygeoip
 
 
+# Rawdata that contains the mapping between Ip's and geolocations.
+rawdata = pygeoip.GeoIP("./sushi_network/resources/GeoLiteCity.dat")
+# function that returns the latitude and longitude of the received IP
+def ipquery(ip):
+    data = rawdata.record_by_name(ip)
+    #country = data['country_name']
+    #city = data['city']
+    lat = data['latitude']
+    longi = data['longitude']
+    #print '[x] '+str(city)+',' +str(country)
+    #print '[x] Latitude: '+str(lat)+ ', Longitude: '+ str(longi)
+    return (lat,longi)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 class HomeView(generic.ListView):
     template_name = 'home.html'
@@ -10,7 +31,10 @@ class HomeView(generic.ListView):
                                    client_secret="ZSDRDRBEZHO4DUH5TWS2DQMOUP1MMBBMEZJBNDZZVXTUFZX1",
                                    redirect_uri="http://www.icogroup.com")
     def get_queryset(self):
+        geolocation = ipquery('8.8.8.8')
 
+
+        print geolocation
         return ""
 
 
