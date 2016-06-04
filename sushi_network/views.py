737630686db5ed18565eb951556904025f6ecd84
Luis Fernandes <sednanref2004@gmail.com>
2016-06-04 00:33:55 -0400

    Initial commit, Python and django setup.

---
+++ sushi_network/views.py
@@ -0,0 +1,16 @@
+from django.views import generic
+import foursquare
+import
+
+
+class HomeView(generic.ListView):
+    template_name = 'home.html'
+    context_object_name = 'home_page'
+    client = foursquare.Foursquare(client_id="MSB3H0C14BLRSSAAYGELVLAAWVKWUL5DXYVMWF2UJPUVZOMC",
+                                   client_secret="ZSDRDRBEZHO4DUH5TWS2DQMOUP1MMBBMEZJBNDZZVXTUFZX1",
+                                   redirect_uri="http://www.icogroup.com")
+    def get_queryset(self):
+
+        return ""
+
+
